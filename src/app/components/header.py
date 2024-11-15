47c912e9267b8d88cab6da8607b76cb809a01f28
Marco Fura <mjfura27@gmail.com>
2024-11-14 21:03:05 -0300

    feat add app

---
+++ src/app/components/header.py
@@ -0,0 +1,5 @@
+import streamlit as st
+def header():
+    st.title("Actividad 01")
+    st.subheader("Carga de Documentos")
+    st.write("En esta sección se cargará un archivo pdf, se preprocesará y se guardará en un Bucket. Detallaremos todo el flujo paso a paso.")
